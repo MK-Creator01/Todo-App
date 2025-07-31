b6f06c28f5b8ed370bec317421e9db8c7aa9040d
MK-Creator01 <mayurkharadi927@gmail.com>
2025-07-31 15:10:51 +0530

    add the image and task

---
+++ todo.js
@@ -0,0 +1,14 @@
+// prompt("");
+
+
+function AddItem() {
+  <div class="items">
+  <select name="cars" id="option">
+      <option value="Panding">&#xf079;</option>
+      <option value="Done">&#10004;</option>
+      <option value="Skip">&#10680;</option>
+  </select>
+  <p> </p>
+</div>
+document.getElementById("add").innerHtml;
+}
