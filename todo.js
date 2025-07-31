ce3e735c156bf8281b79f702254d8c4a3cb5709d
MK-Creator01 <mayurkharadi927@gmail.com>
2025-07-31 16:33:57 +0530

    add the image and task2

--- todo.js
+++ todo.js
@@ -2,13 +2,18 @@
 
 
 function AddItem() {
-  <div class="items">
-  <select name="cars" id="option">
-      <option value="Panding">&#xf079;</option>
-      <option value="Done">&#10004;</option>
-      <option value="Skip">&#10680;</option>
-  </select>
-  <p> </p>
-</div>
-document.getElementById("add").innerHtml;
+  // const para = document.createElement("p");
+  // const node = document.createTextNode("This is new.");
+  // para.appendChild(node);
+  // const element = document.getElementById("div1");
+  // element.appendChild(para);
+  var x = document.getElementById("myText").value;
+  console.log(x);
+  document.getElementById("demo").innerHTML = x;
 }
+
+
+// function myFunction() {
+//   var x = document.getElementById("myText").value;
+//   document.getElementById("demo").innerHTML = x;
+// }
